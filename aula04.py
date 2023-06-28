9671cc53c25066ac400c5958a8cc3753ccb38c26
Guilherme-Schardong <86854610+Guilherme-Schardong@users.noreply.github.com>
2023-06-28 19:28:53 -0300

    Add files via upload

---
+++ aula04.py
@@ -0,0 +1,25 @@
+# faça um progrma que peça ao usuario qual operação 
+# matematica ele deseja realizar entre ( +, -, /, *)
+# apos selecionada o usuario deve informar os valosres
+#  para realizar as operações e por final 
+# printar o resultado da operação
+# as operações deverão ser feitas em outro arquivo
+#  e importadas para o arquivo principal
+
+
+
+
+
+# nome = 'Guilherme.schardong'
+
+# dicionario = {"nome": "carro", "logica": True, "numeroInt": 2, "numeroFloat": 3.5}
+
+# print('carro' in dicionario)
+
+
+
+# faça um programa que simule um dado de 6 lados e que peça ao usuario
+# o numero de vezes que ele deseja lançar o dado, 
+# no final o programa deverá dizer quantas vezes caiu cada lado no numero
+# de vezes que o jogador pediu para jogar
+
---
+++ aula05.py
@@ -0,0 +1,27 @@
+
+
+# def saudacao(nome, sobrenome,segundoNome=None):
+#     print(segundoNome)
+#     print(nome)
+#     print(sobrenome)
+#     print('ola a todos meu nome é {} {}'.format(nome,sobrenome))
+
+
+# a = 'Guilherme'
+# b = 'Schardong'
+
+# saudacao(sobrenome=b,nome=a)
+
+
+
+# frutas = ['maçã', 'banana', 'laranja']
+# print("Lista original:", frutas)
+
+# frutas.append(a)
+
+# print(frutas)
+
+# a = 10
+# b = 5
+# c = b-a
+# print(abs(b-a))
