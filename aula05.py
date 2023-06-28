050d7df4e233ec3e5bb4da95515bd4a69a429145
Guilherme-Schardong <86854610+Guilherme-Schardong@users.noreply.github.com>
2023-06-28 20:48:19 -0300

    Add files via upload

--- aula05.py
+++ aula05.py
@@ -25,3 +25,31 @@
 # b = 5
 # c = b-a
 # print(abs(b-a))
+
+
+
+#crie um jogo de pedra papel e tesoura o jogo deve dizer se 
+#o usuario ganhou ou perdeu e se empatar         
+#deve pedir ao usurio para selecionar uma opção nova
+
+
+# crie um programa que conte quantas vogais a na string passada pelomusuario
+
+
+
+import random
+
+def gerador_de_senhas():
+    caracteres = 'abcdefghijklmnopqrstuvyxzABCDEFGHIJKLMNOPQRSTUVYXZ0123456789'
+    tamanho = int(input('Qual o tamanha da senha que você deseja: '))
+    senha=''
+
+    for i in range(tamanho):
+        caract = random.choice(caracteres)
+        senha += caract
+    
+    print(senha)
+
+if(__name__=='__main__'):
+    gerador_de_senhas()
+
