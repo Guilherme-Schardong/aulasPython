0c62c69e61177cdf554d45f989e096e813cf0c27
Guilherme-Schardong <86854610+Guilherme-Schardong@users.noreply.github.com>
2023-06-30 19:41:58 -0300

    Add files via upload

---
+++ aula06.py
@@ -0,0 +1,107 @@
+# food = ["pasta", "pizza", "lasagna"]
+
+# serv = food.copy()
+
+# print(serv)
+
+
+#clear()
+#Clear significa limpar. Este método apaga todos os elementos de uma lista.
+
+# food = ["pasta", "pizza", "lasagna"]
+# food.clear()
+
+# print(food)
+
+
+#count()
+#Count significa contar. Este método retorna o número de elementos que contenham o valor especificado.
+
+# food = ["pasta", "pizza", "lasagna"]
+# x = food.count("pizza")
+
+
+
+
+
+#Extend significa estender. Este método adiciona os elementos de uma lista ao final de outra lista.
+
+food = ["pasta", "pizza", "lasagna"]
+# dessert = ["chocolate", "gelato"]
+# food.extend(dessert)
+
+# print(food)
+
+
+#index()
+#Index significa índice. Este método retorna a posição do primeiro elemento que contenha o valor especificado.
+
+# food = ["pasta", "pizza", "lasagna"]
+# x = food.index("pizza")
+
+# print(x)
+
+
+#insert()
+#Insert significa inserir. Este método adiciona um elemento a uma posição específica.
+
+# food = ["pasta", "pizza", "lasagna"]
+# food.insert(1, "spaghetti")
+
+# print(food)
+
+
+#pop()
+#Pop significa estourar. Este método remove um elemento de uma posição específica.
+
+# food = ["pasta", "pizza", "lasagna"]
+# food.pop(1)
+
+# print(food)
+
+
+#remove()
+#Remove significa remover. Este método remove o primeiro item com o valor especificado.
+
+# food = ["pasta", "pizza", "lasagna"]
+# food.remove("lasagna")
+
+# print(food)
+
+
+#reverse()
+#Reverse significa reverter. Este método reverte a ordem da lista.
+
+# food = ["pasta", "pizza", "lasagna"]
+# food.reverse()
+# print(food)
+
+
+#[::-1]
+
+# food = ["pasta", "pizza", "lasagna"]
+
+# foodNova = food[::-1]
+
+# print(foodNova)
+
+
+#sort()
+#Sort significa ordenar. Este método ordena a lista alfabeticamente.
+#food = ["pasta", "pizza", "lasagna"]
+#food.sort()
+#print(food)
+
+#sorted()
+
+# food = ["pasta", "pizza", "lasagna"]
+
+# ordenada = sorted(food)
+
+# print(food)
+# print(ordenada)
+
+
+# food = ["pasta", "pizza", "lasagna"]
+
+# print(food[-1])
