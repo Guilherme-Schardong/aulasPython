f20e12c2c4855707db652c4e3ad5b4e1176f00f4
Guilherme-Schardong <86854610+Guilherme-Schardong@users.noreply.github.com>
2023-08-16 19:08:19 -0300

    Add files via upload

---
+++ glo.py
@@ -0,0 +1,19 @@
+
+# valor = 'valor'
+
+# def func():
+#     print(valor)
+
+# def func2():
+#     global valor 
+#     valor ='outro valor'
+#     print(valor)
+
+# func()
+# func2()
+# print(valor)
+
+
+entrada = str(input('digite uma sentença: '))
+
+print(eval(entrada))
